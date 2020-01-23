dda7bad58b1042f7673d4f313d7e561b7106c83d
Paweł Marchewka <pawel.marchewka@codilime.com>
2020-01-23 10:55:05 +0100

    Enable e2e tests in CI (#34)
    * Enable e2e tests in CI
    
    * Code clean up
    
    * Removed unused variable
    
    * Changed node

--- test/env/create_testenv.sh
+++ test/env/create_testenv.sh
@@ -1,12 +1,10 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -o errexit
 
 # desired cluster name; default is "kind"
 KIND_CLUSTER_NAME="${KIND_CLUSTER_NAME:-kind}"
-EXTERNAL_INSECURE_REGISTRY="${EXTERNAL_INSECURE_REGISTRY:-127.0.0.1:5000}"
 INTERNAL_INSECURE_REGISTRY_PORT="${INTERNAL_INSECURE_REGISTRY_PORT:-5000}"
-
 
 # create registry container unless it already exists
 reg_name='kind-registry'
@@ -18,23 +16,7 @@ if [ "${running}" != 'true' ]; then
     registry:2
 fi
 
-# create a cluster with the local registry enabled in containerd
-cat <<EOF | kind create cluster --name "${KIND_CLUSTER_NAME}" --config=-
-kind: Cluster
-apiVersion: kind.x-k8s.io/v1alpha4
-containerdConfigPatches: 
-- |-
-  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."registry:5000"]
-    endpoint = ["http://registry:5000"]
-  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."${EXTERNAL_INSECURE_REGISTRY}"]
-    endpoint = ["http://${EXTERNAL_INSECURE_REGISTRY}"]
-EOF
-
-# add the registry to /etc/hosts on each node
-ip_fmt='{{.NetworkSettings.IPAddress}}'
-cmd="echo $(docker inspect -f "${ip_fmt}" "${reg_name}") registry >> /etc/hosts"
-for node in $(kind get nodes --name "${KIND_CLUSTER_NAME}"); do
-  docker exec "${node}" sh -c "${cmd}"
-done
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+${DIR}/create_k8s_cluster.sh $KIND_CLUSTER_NAME $(docker inspect -f "{{.NetworkSettings.IPAddress}}" "${reg_name}")
 
 docker cp -L /etc/localtime "${KIND_CLUSTER_NAME}"-control-plane:/etc/localtime
