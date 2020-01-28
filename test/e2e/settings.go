bc9bc04135f5029dbc5034a5aca12dcbf67f9c58
Jacek Olszak <jacek.olszak@codilime.com>
2020-01-28 14:16:23 +0100

    Increas timeouts

--- test/e2e/settings.go
+++ test/e2e/settings.go
@@ -3,8 +3,8 @@ package e2e
 import "time"
 
 var (
-	retryInterval        = time.Second * 5
-	waitTimeout          = time.Second * 120
-	cleanupRetryInterval = time.Second * 1
-	cleanupTimeout       = time.Second * 5
+	retryInterval        = time.Second * 10
+	waitTimeout          = time.Second * 240
+	cleanupRetryInterval = time.Second * 2
+	cleanupTimeout       = time.Second * 10
 )
