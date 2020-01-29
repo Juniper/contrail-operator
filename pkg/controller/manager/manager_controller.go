adac1340a42db31e68058585a412924a09559e98
Tomasz Nowak <tomasz.nowak@codilime.com>
2020-01-29 13:15:23 +0100

    Set Manager.Status.Memcached in manager controller

--- pkg/controller/manager/manager_controller.go
+++ pkg/controller/manager/manager_controller.go
@@ -37,6 +37,7 @@ var resourcesList = []runtime.Object{
 	&v1alpha1.ContrailCommand{},
 	&v1alpha1.Keystone{},
 	&v1alpha1.Swift{},
+	&v1alpha1.Memcached{},
 }
 
 /**
@@ -1505,5 +1506,8 @@ func (r *ReconcileManager) processMemcached(manager *v1alpha1.Manager) error {
 		memcached.Spec = manager.Spec.Services.Memcached.Spec
 		return controllerutil.SetControllerReference(manager, memcached, r.scheme)
 	})
+	status := &v1alpha1.ServiceStatus{}
+	status.Active = &memcached.Status.Active
+	manager.Status.Memcached = status
 	return err
 }
