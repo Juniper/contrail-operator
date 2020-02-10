33ac489203654de1fe0eb1e7ef888d3d4382002d
Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
2020-02-10 10:09:56 +0100

    Add todo comment for auth config

--- pkg/apis/contrail/v1alpha1/config_types.go
+++ pkg/apis/contrail/v1alpha1/config_types.go
@@ -229,8 +229,8 @@ func (c *Config) InstanceConfiguration(request reconcile.Request,
 		})
 		data["devicemanager."+podList.Items[idx].Status.PodIP] = configDevicemanagerConfigBuffer.String()
 
+		// TODO: this config should be stored in secret.
 		configAuth := c.AuthParameters()
-
 		var configKeystoneAuthConfBuffer bytes.Buffer
 		configtemplates.ConfigKeystoneAuthConf.Execute(&configKeystoneAuthConfBuffer, struct {
 			AdminUsername string
