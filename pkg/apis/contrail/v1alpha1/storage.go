95c3ee76eca195b06f43f79ec30276f01789e265
Jacek Olszak <jacek.olszak@codilime.com>
2020-02-24 11:02:26 +0100

    Add storage to controllers

--- pkg/apis/contrail/v1alpha1/storage.go
+++ pkg/apis/contrail/v1alpha1/storage.go
@@ -5,9 +5,8 @@ import (
 )
 
 type Storage struct {
-	// The only reason we don't use resource.Quantity directly is we can't have regexp for different type than string
 	// +kubebuilder:validation:Pattern=^([0-9]+)([KMGTPE]i)?$
-	Size string `json:"size,omitempty"`
+	Size string `json:"size,omitempty"` // The only reason we don't use resource.Quantity directly is we can't have regexp for different type than string
 	Path string `json:"path,omitempty"`
 }
 
