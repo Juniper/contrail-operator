9d06e1dbe535c645272784cfee0f1562a706d35c
Pawel Marchewka <pawel.marchewka@codilime.com>
2020-03-20 13:27:09 +0100

    Removed incorrect comment

--- test/e2e/manager_test.go
+++ test/e2e/manager_test.go
@@ -281,8 +281,8 @@ func getManager(namespace string, replicas int32, hostNetwork bool, versionMap m
 
 							Containers: map[string]*v1alpha1.Container{
 								"api":               &v1alpha1.Container{Image: "registry:5000/contrail-controller-config-api:" + versionMap["config"]},
-								"devicemanager":     &v1alpha1.Container{Image: "registry:5000/contrail-controller-config-devicemgr:master.1115"}, // Using custom dev version until required changes are merged upstream
-								"dnsmasq":           &v1alpha1.Container{Image: "registry:5000/contrail-controller-config-dnsmasq:dev"},           // Using custom dev version until required changes are merged upstream
+								"devicemanager":     &v1alpha1.Container{Image: "registry:5000/contrail-controller-config-devicemgr:master.1115"},
+								"dnsmasq":           &v1alpha1.Container{Image: "registry:5000/contrail-controller-config-dnsmasq:dev"}, // Using custom dev version until required changes are merged upstream
 								"schematransformer": &v1alpha1.Container{Image: "registry:5000/contrail-controller-config-schema:" + versionMap["config"]},
 								"servicemonitor":    &v1alpha1.Container{Image: "registry:5000/contrail-controller-config-svcmonitor:" + versionMap["config"]},
 								"analyticsapi":      &v1alpha1.Container{Image: "registry:5000/contrail-analytics-api:" + versionMap["config"]},
