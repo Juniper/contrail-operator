591c6dfad4322806d44b208e6a691071ead7c4e7
Tomasz Nowak <tomasz.nowak@codilime.com>
2020-03-20 11:20:20 +0100

    Do not build contrail-provisioner in update_local_registry.sh

--- test/env/update_local_registry.sh
+++ test/env/update_local_registry.sh
@@ -11,16 +11,6 @@ pull_image()
     docker push $LOCAL_REPO/$2
 }
 
-push_local_images()
-{
-  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-  make -f $DIR/../../Makefile provisioner && \
-  docker tag contrail-provisioner:debug $LOCAL_REPO/contrail-provisioner:latest && \
-  docker push $LOCAL_REPO/contrail-provisioner:latest
-}
-
-push_local_images
-
 while read line; do
 	pull_image opencontrailnightly "${line}"
 done <<EOF
