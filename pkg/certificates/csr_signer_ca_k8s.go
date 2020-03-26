f59a11b6fd1806c131ee9c2360be73bf87367c65
Piotr Sykulski <piotr.sykulski@codilime.com>
2020-03-26 17:24:51 +0100

    Implemented csr signer CA value extraction in a bare k8s cluster

--- pkg/certificates/csr_signer_ca_k8s.go
+++ pkg/certificates/csr_signer_ca_k8s.go
@@ -3,6 +3,8 @@ package certificates
 import (
 	"errors"
 
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	typedCorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 )
 
@@ -13,6 +15,34 @@ type CSRSignerCAK8s struct {
 
 // TODO
 func (c CSRSignerCAK8s) CSRSignerCA() (string, error) {
-	_ = c
-	return "", errors.New("used empty k8s implementation")
+	kubeSystemServiceAccountsClient := c.Client.ServiceAccounts("kube-system")
+	defaultServiceAccount, err := kubeSystemServiceAccountsClient.Get("default", metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	accountTokenSecret, err := c.getServiceAccountTokenSecret(defaultServiceAccount)
+	if err != nil {
+		return "", err
+	}
+
+	caData, ok := accountTokenSecret.Data["ca.crt"]
+	if !ok {
+		return "", errors.New("ca.crt field not found in the accountToken")
+	}
+	return string(caData), nil
+}
+
+func (c CSRSignerCAK8s) getServiceAccountTokenSecret(serviceAccount *corev1.ServiceAccount) (*corev1.Secret, error) {
+	secretsClient := c.Client.Secrets(serviceAccount.Namespace)
+	for _, secretRef := range serviceAccount.Secrets {
+		secret, err := secretsClient.Get(secretRef.Name, metav1.GetOptions{})
+		if err != nil {
+			return nil, err
+		}
+		if secret.Type == corev1.SecretTypeServiceAccountToken {
+			return secret, nil
+		}
+	}
+	return nil, errors.New("No Secret of the SecretTypeServiceAccount found.")
 }
