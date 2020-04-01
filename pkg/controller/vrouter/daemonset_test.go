43cb63d6c4a7196c8f3812f539db216550d0e17a
Dysproz <krasuski.szymon.piotr@gmail.com>
2020-04-01 13:17:25 +0200

    more multiline

--- pkg/controller/vrouter/daemonset_test.go
+++ pkg/controller/vrouter/daemonset_test.go
@@ -20,8 +20,12 @@ func TestGetDaemonset(t *testing.T) {
 	ds, err := vrouter.GetDaemonset(testCNIDirs)
 	assert.NoError(t, err)
 	pathType := v1.HostPathType("")
-	assert.Contains(t, ds.Spec.Template.Spec.Volumes, v1.Volume{Name: "cni-bin",
-		VolumeSource: v1.VolumeSource{HostPath: &v1.HostPathVolumeSource{Path: testBinariesPath, Type: &pathType}}})
-	assert.Contains(t, ds.Spec.Template.Spec.Volumes, v1.Volume{Name: "cni-config-files",
-		VolumeSource: v1.VolumeSource{HostPath: &v1.HostPathVolumeSource{Path: testConfigPath, Type: &pathType}}})
+	assert.Contains(t, ds.Spec.Template.Spec.Volumes,
+		v1.Volume{Name: "cni-bin",
+			VolumeSource: v1.VolumeSource{HostPath: &v1.HostPathVolumeSource{Path: testBinariesPath,
+				Type: &pathType}}})
+	assert.Contains(t, ds.Spec.Template.Spec.Volumes,
+		v1.Volume{Name: "cni-config-files",
+			VolumeSource: v1.VolumeSource{HostPath: &v1.HostPathVolumeSource{Path: testConfigPath,
+				Type: &pathType}}})
 }
