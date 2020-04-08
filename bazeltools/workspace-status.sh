f17be67bf2625496a90119be81cd4bf870f11a40
Michael Henkel <mhenkel@juniper.net>
2020-04-08 13:44:30 -0700

    test

--- bazeltools/workspace-status.sh
+++ bazeltools/workspace-status.sh
@@ -2,4 +2,7 @@
 set -eu
 
 # Execute workspace-status.py from the same directory as this bash script
-"${0%.sh}.py"
+#"${0%.sh}.py"
+
+echo BUILD_SCM_REVISION $(git rev-parse --short HEAD)
+echo BUILD_SCM_BRANCH $(git rev-parse --abbrev-ref HEAD)
