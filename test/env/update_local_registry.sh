462f77423767e91d273af1018aa28e879db13edc
Piotr Sykulski <piotr.sykulski@codilime.com>
2020-04-16 10:39:39 +0200

    Go back to using the master.1115 tag (#134)
    * Go back to using the master.1115 tag.
    
    * Increase wait timeout from 120 to 180 seconds

--- test/env/update_local_registry.sh
+++ test/env/update_local_registry.sh
@@ -71,7 +71,7 @@ while read line; do
 	pull_image kaweue "${line}"
 done <<EOF
 contrail-statusmonitor:debug
-contrail-provisioner:master.1115-keystone-tls
+contrail-provisioner:master.1115
 EOF
 
 pull_image hub.juniper.net/contrail-nightly contrail-command:master.1115
