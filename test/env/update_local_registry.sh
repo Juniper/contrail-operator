562fcca1db998c73e4febf804062e60f245f647f
Mateusz Figurski <mateusz.figurski@codilime.com>
2020-04-17 12:34:13 +0200

    change config-dnsmasq container tag in pulling script

--- test/env/update_local_registry.sh
+++ test/env/update_local_registry.sh
@@ -75,4 +75,4 @@ contrail-provisioner:master.1115
 EOF
 
 pull_image hub.juniper.net/contrail-nightly contrail-command:master.1175
-pull_image hub.juniper.net/contrail-nightly contrail-controller-config-dnsmasq:master.1115
+pull_image hub.juniper.net/contrail-nightly contrail-controller-config-dnsmasq:master.1175
