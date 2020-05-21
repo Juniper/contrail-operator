19938c01c4ced25bd9cde479e68db04e2412b7ac
Piotr Sykulski <psykulski@juniper.net>
2020-05-21 14:37:04 +0200

    Unit tests for vrotuer action map creation

--- contrail-provisioner/vrouter_nodes/vrouter_nodes_test.go
+++ contrail-provisioner/vrouter_nodes/vrouter_nodes_test.go
@@ -1,2 +1,81 @@
 package vrouter_nodes
 
+import (
+	"testing"
+
+	"github.com/Juniper/contrail-operator/contrail-provisioner/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateVrouterNodesActionMap(t *testing.T) {
+	requiredNodeOne := &types.VrouterNode{
+		IPAddress: "1.1.1.1",
+		Hostname:  "first-node",
+	}
+	requiredNodeTwo := &types.VrouterNode{
+		IPAddress: "2.2.2.2",
+		Hostname:  "second-node",
+	}
+	modifiedRequiredNodeTwo := &types.VrouterNode{
+		IPAddress: "2.2.2.3",
+		Hostname:  "second-node",
+	}
+	apiServerNodeOne := &types.VrouterNode{
+		IPAddress: "1.1.1.1",
+		Hostname:  "first-node",
+	}
+	apiServerNodeTwo := &types.VrouterNode{
+		IPAddress: "2.2.2.2",
+		Hostname:  "second-node",
+	}
+
+	testCases := []struct {
+		name              string
+		nodesInApiServer  []*types.VrouterNode
+		requiredNodes     []*types.VrouterNode
+		expectedActionMap map[string]NodeWithAction
+	}{
+		{
+			name:             "Create action for all required nodes if none in the Api Server",
+			nodesInApiServer: []*types.VrouterNode{},
+			requiredNodes:    []*types.VrouterNode{requiredNodeOne, requiredNodeTwo},
+			expectedActionMap: map[string]NodeWithAction{
+				"first-node":  {requiredNodeOne, createAction},
+				"second-node": {requiredNodeTwo, createAction},
+			},
+		},
+		{
+			name:             "Noop action for nodes in the Api Server",
+			nodesInApiServer: []*types.VrouterNode{apiServerNodeTwo},
+			requiredNodes:    []*types.VrouterNode{requiredNodeOne, requiredNodeTwo},
+			expectedActionMap: map[string]NodeWithAction{
+				"first-node":  {requiredNodeOne, createAction},
+				"second-node": {requiredNodeTwo, noopAction},
+			},
+		},
+		{
+			name:             "Delete action for not required nodes in the Api Server",
+			nodesInApiServer: []*types.VrouterNode{apiServerNodeTwo, apiServerNodeOne},
+			requiredNodes:    []*types.VrouterNode{requiredNodeTwo},
+			expectedActionMap: map[string]NodeWithAction{
+				"first-node":  {requiredNodeOne, deleteAction},
+				"second-node": {requiredNodeTwo, noopAction},
+			},
+		},
+		{
+			name:             "Update action for modified node",
+			nodesInApiServer: []*types.VrouterNode{apiServerNodeTwo, apiServerNodeOne},
+			requiredNodes:    []*types.VrouterNode{requiredNodeOne, modifiedRequiredNodeTwo},
+			expectedActionMap: map[string]NodeWithAction{
+				"first-node":  {requiredNodeOne, noopAction},
+				"second-node": {modifiedRequiredNodeTwo, updateAction},
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			actualActionMap := createVrouterNodesActionMap(testCase.nodesInApiServer, testCase.requiredNodes)
+			assert.Equal(t, testCase.expectedActionMap, actualActionMap)
+		})
+	}
+}
