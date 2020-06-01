e1ce78bbd926dd687f5327df24bd38b0d1496d2b
Pawel Marchewka <pawel.marchewka@codilime.com>
2020-06-01 14:40:28 +0200

    Start HA cluster

--- test/gcb/run_e2e_test.sh
+++ test/gcb/run_e2e_test.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
 
 kind delete cluster
-kind create cluster
-IP=$(docker inspect -f "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" kind-control-plane)
+kind create cluster --config "/workspace/test/gcb/cluster.yaml"
+IP=$(docker inspect -f "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" kind-external-load-balancer)
 echo $IP
 kubectl config set-cluster kind-kind --server=https://$IP:6443 --insecure-skip-tls-verify
+sleep 20
 kubectl cluster-info
-kind delete cluster;
+kubectl get nodes -o wide
+kind get nodes
+kind delete cluster
