bffacce08c0de6de354079a38c39c4e4b14cca5e
Piotr Sykulski <psykulski@juniper.net>
2020-06-03 13:10:30 +0200

    Use /usr/local/bin instead of /bin in vrouter containers volumes (#218)
    * On Redhat CoreOS system (used by Openshift) /bin is a read only filesystem. Currently pod's of the vrouter daemonset have a volume with hostpath /bin. It is mounted to the /host/bin directory. This volume is used only by the vrouterkernelinit container, that uses it to copy the vif binary to the host.
    https://github.com/Juniper/contrail-container-builder/blob/master/containers/vrouter/kernel-build-init/entrypoint.sh#L44
    
    As one can see by searching for the /host/bin path, it is the only place where this path is used.
    https://github.com/Juniper/contrail-container-builder/search?q=%2Fhost%2Fbin&unscoped_q=%2Fhost%2Fbin
    
    Because /bin is read-only on RHCOS, copying of the vif tool fails and it cannot be used by the vrouter agent container.
    
    This PR changes the volumue mounted to the /host/bin directory from the /bin volume to the /usr/local/bin volume, which is a writable directory on RHCOS.

--- pkg/controller/vrouter/daemonset.go
+++ pkg/controller/vrouter/daemonset.go
@@ -87,7 +87,7 @@ func GetDaemonset(cniDir CniDirs) *apps.DaemonSet {
 					MountPath: "/etc/sysconfig/network-scripts",
 				},
 				core.VolumeMount{
-					Name:      "host-bin",
+					Name:      "host-usr-local-bin",
 					MountPath: "/host/bin",
 				},
 				core.VolumeMount{
@@ -127,7 +127,7 @@ func GetDaemonset(cniDir CniDirs) *apps.DaemonSet {
 					MountPath: "/etc/sysconfig/network-scripts",
 				},
 				core.VolumeMount{
-					Name:      "host-bin",
+					Name:      "host-usr-local-bin",
 					MountPath: "/host/bin",
 				},
 				core.VolumeMount{
@@ -288,14 +288,6 @@ func GetDaemonset(cniDir CniDirs) *apps.DaemonSet {
 			VolumeSource: core.VolumeSource{
 				HostPath: &core.HostPathVolumeSource{
 					Path: "/usr/src",
-				},
-			},
-		},
-		core.Volume{
-			Name: "host-bin",
-			VolumeSource: core.VolumeSource{
-				HostPath: &core.HostPathVolumeSource{
-					Path: "/bin",
 				},
 			},
 		},
