13d61c05b85c260aabdfa5c553b78f17cf74046d
Piotr Sykulski <psykulski@juniper.net>
2020-06-24 14:07:35 +0200

    Moved vroutercni container to init containers and updated mount paths and volumes to the current contrail-kubernetes-cni-init container

--- pkg/controller/vrouter/vrouter_controller.go
+++ pkg/controller/vrouter/vrouter_controller.go
@@ -425,41 +425,6 @@ func (r *ReconcileVrouter) Reconcile(request reconcile.Request) (reconcile.Resul
 				},
 			}}
 		}
-		if container.Name == "vroutercni" {
-			command := []string{"sh", "-c",
-				"cp loopback /host/opt_cni_bin/ && cp /contrailcni_client /host/opt_cni_bin/contrail-k8s-cni && mkdir /host/etc_cni/net.d ||true && mkdir /var/run/contrail || true && mkdir -p /var/lib/contrail/ports/vm || true && cp /etc/mycontrail/10-contrail.conf /host/etc_cni/net.d/10-contrail.conf && /contrailcni_server"}
-			//command = []string{"sh", "-c", "while true; do echo hello; sleep 10;done"}
-			instanceContainer := utils.GetContainerFromList(container.Name, instance.Spec.ServiceConfiguration.Containers)
-			if instanceContainer.Command == nil {
-				(&daemonSet.Spec.Template.Spec.Containers[idx]).Command = command
-			} else {
-				(&daemonSet.Spec.Template.Spec.Containers[idx]).Command = instanceContainer.Command
-			}
-
-			volumeMountList := []corev1.VolumeMount{}
-			if len((&daemonSet.Spec.Template.Spec.Containers[idx]).VolumeMounts) > 0 {
-				volumeMountList = (&daemonSet.Spec.Template.Spec.Containers[idx]).VolumeMounts
-			}
-			volumeMount := corev1.VolumeMount{
-				Name:      request.Name + "-" + instanceType + "-volume",
-				MountPath: "/etc/mycontrail",
-			}
-			volumeMountList = append(volumeMountList, volumeMount)
-			volumeMount = corev1.VolumeMount{
-				Name:      request.Name + "-secret-certificates",
-				MountPath: "/etc/certificates",
-			}
-			volumeMountList = append(volumeMountList, volumeMount)
-			(&daemonSet.Spec.Template.Spec.Containers[idx]).VolumeMounts = volumeMountList
-			(&daemonSet.Spec.Template.Spec.Containers[idx]).Image = instanceContainer.Image
-			(&daemonSet.Spec.Template.Spec.Containers[idx]).EnvFrom = []corev1.EnvFromSource{{
-				ConfigMapRef: &corev1.ConfigMapEnvSource{
-					LocalObjectReference: corev1.LocalObjectReference{
-						Name: request.Name + "-" + instanceType + "-configmap-1",
-					},
-				},
-			}}
-		}
 		if container.Name == "nodemanager" {
 			if nodemgr {
 				command := []string{"bash", "-c",
@@ -523,6 +488,41 @@ func (r *ReconcileVrouter) Reconcile(request reconcile.Request) (reconcile.Resul
 			if instance.Spec.ServiceConfiguration.Distribution != nil || instance.Spec.ServiceConfiguration.Distribution == &ubuntu {
 				(&daemonSet.Spec.Template.Spec.InitContainers[idx]).Image = instanceContainer.Image
 			}
+		}
+		if container.Name == "vroutercni" {
+			command := []string{"sh", "-c",
+				"cp loopback /host/opt_cni_bin/ && cp /contrailcni_client /host/opt_cni_bin/contrail-k8s-cni && mkdir /host/etc_cni/net.d ||true && mkdir /var/run/contrail || true && mkdir -p /var/lib/contrail/ports/vm || true && cp /etc/mycontrail/10-contrail.conf /host/etc_cni/net.d/10-contrail.conf && /contrailcni_server"}
+			//command = []string{"sh", "-c", "while true; do echo hello; sleep 10;done"}
+			instanceContainer := utils.GetContainerFromList(container.Name, instance.Spec.ServiceConfiguration.Containers)
+			if instanceContainer.Command == nil {
+				(&daemonSet.Spec.Template.Spec.Containers[idx]).Command = command
+			} else {
+				(&daemonSet.Spec.Template.Spec.Containers[idx]).Command = instanceContainer.Command
+			}
+
+			volumeMountList := []corev1.VolumeMount{}
+			if len((&daemonSet.Spec.Template.Spec.Containers[idx]).VolumeMounts) > 0 {
+				volumeMountList = (&daemonSet.Spec.Template.Spec.Containers[idx]).VolumeMounts
+			}
+			volumeMount := corev1.VolumeMount{
+				Name:      request.Name + "-" + instanceType + "-volume",
+				MountPath: "/etc/mycontrail",
+			}
+			volumeMountList = append(volumeMountList, volumeMount)
+			volumeMount = corev1.VolumeMount{
+				Name:      request.Name + "-secret-certificates",
+				MountPath: "/etc/certificates",
+			}
+			volumeMountList = append(volumeMountList, volumeMount)
+			(&daemonSet.Spec.Template.Spec.Containers[idx]).VolumeMounts = volumeMountList
+			(&daemonSet.Spec.Template.Spec.Containers[idx]).Image = instanceContainer.Image
+			(&daemonSet.Spec.Template.Spec.Containers[idx]).EnvFrom = []corev1.EnvFromSource{{
+				ConfigMapRef: &corev1.ConfigMapEnvSource{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: request.Name + "-" + instanceType + "-configmap-1",
+					},
+				},
+			}}
 		}
 
 	}
