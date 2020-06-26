e0eaac1e77a97a62dc49b463798ef57e5dd659b2
Pawel Marchewka <pawel.marchewka@codilime.com>
2020-06-26 13:07:15 +0200

    CI improvments for HA

---
+++ test/env/run_e2e_tests.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+BUILD_SCM_BRANCH=${BUILD_SCM_BRANCH:-"master"}
+BUILD_SCM_REVISION=${BUILD_SCM_REVISION:-"latest"}
+
+kubectl apply -f deploy/cluster_role.yaml
+kubectl apply -f deploy/cluster_role_binding.yaml
+operator-sdk test local ./test/e2e/ --image registry:5000/contrail-operator/engprod-269421/contrail-operator:${BUILD_SCM_BRANCH}.${BUILD_SCM_REVISION} --namespace contrail --go-test-flags "-v -timeout=30m"
+
+kubectl delete -f deploy/cluster_role.yaml
+kubectl delete -f deploy/cluster_role_binding.yaml
