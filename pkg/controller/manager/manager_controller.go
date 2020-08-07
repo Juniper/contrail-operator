d95254d15f61286b87e1be91aaf23a4f3e8465da
vijayrap <vraparla@juniper.net>
2020-08-07 01:05:14 -0700

    Added changes for contrailstatusmonitor

--- pkg/controller/manager/manager_controller.go
+++ pkg/controller/manager/manager_controller.go
@@ -43,6 +43,7 @@ var resourcesList = []runtime.Object{
 	&v1alpha1.Memcached{},
 	&v1alpha1.Vrouter{},
 	&v1alpha1.Kubemanager{},
+    &v1alpha1.Contrailmonitor{},
 	&corev1.ConfigMap{},
 }
 
@@ -1491,19 +1492,22 @@ func (r *ReconcileManager) Reconcile(request reconcile.Request) (reconcile.Resul
 		return reconcile.Result{}, err
 	}
 
-	if err = r.processCommand(instance); err != nil {
-		return reconcile.Result{}, err
-	}
+	// if err = r.processCommand(instance); err != nil {
+	// 	return reconcile.Result{}, err
+	// }
 
 	if err = r.processKeystone(instance); err != nil {
 		return reconcile.Result{}, err
 	}
 
-	if err = r.processSwift(instance); err != nil {
-		return reconcile.Result{}, err
-	}
+	// if err = r.processSwift(instance); err != nil {
+	// 	return reconcile.Result{}, err
+	// }
 
 	if err = r.processMemcached(instance); err != nil {
+		return reconcile.Result{}, err
+	}
+	if err = r.processContrailmonitor(instance); err != nil {
 		return reconcile.Result{}, err
 	}
 
@@ -1644,3 +1648,21 @@ func (r *ReconcileManager) processCSRSignerCaConfigMap(manager *v1alpha1.Manager
 
 	return err
 }
+
+func (r *ReconcileManager) processContrailmonitor(manager *v1alpha1.Manager) error {
+	if manager.Spec.Services.Contrailmonitor == nil {
+		return nil
+	}
+	contrailmonitor := &v1alpha1.Contrailmonitor{}
+	contrailmonitor.ObjectMeta = manager.Spec.Services.Contrailmonitor.ObjectMeta
+	contrailmonitor.ObjectMeta.Namespace = manager.Namespace
+	_, err := controllerutil.CreateOrUpdate(context.Background(), r.client, contrailmonitor, func() error {
+		contrailmonitor.Spec = manager.Spec.Services.Contrailmonitor.Spec
+		return controllerutil.SetControllerReference(manager, contrailmonitor, r.scheme)
+	})
+	status := &v1alpha1.ServiceStatus{}
+	status.Active = &contrailmonitor.Status.Active
+	manager.Status.Contrailmonitor = status
+	return err
+}
+
