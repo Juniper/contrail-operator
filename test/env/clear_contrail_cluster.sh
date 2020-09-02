b6187956e2e39c055b8a54e087891f2e91abdd75
Pawel Marchewka <pawel.marchewka@codilime.com>
2020-09-02 07:49:29 +0200

    Don't check isUpgrading state since controllers are notified about its change

--- test/env/clear_contrail_cluster.sh
+++ test/env/clear_contrail_cluster.sh
@@ -7,7 +7,7 @@ kubectl delete --wait=true -f deploy/cluster.yaml
 kubectl delete -f ../../deploy/1-create-operator.yaml
 kubectl delete pv $(kubectl get pv -o=jsonpath='{.items[?(@.spec.storageClassName=="local-storage")].metadata.name}')
 
-for p in $(docker ps --filter name=kind-control -q)
+for p in $(docker ps --filter name=kind-control --filter name=kind-worker -q)
 do
     docker exec $p rm -rf /var/lib/cassandra
     docker exec $p rm -rf /var/lib/zookeeper
