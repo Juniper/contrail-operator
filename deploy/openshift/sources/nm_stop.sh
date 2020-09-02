3c7f245737b97ba84f2f7e1ae4c4eeaf2834575b
Piotr Sykulski <psykulski@juniper.net>
2020-09-02 15:47:10 +0200

    Moved static hostname scritp to nm_stop.sh

--- deploy/openshift/sources/nm_stop.sh
+++ deploy/openshift/sources/nm_stop.sh
@@ -2,6 +2,25 @@
 
 while true;
 do
+  FQHOSTNAME=$(hostname -f)
+  if [[ $FQHOSTNAME != 'localhost' ]] && [[ $FQHOSTNAME != 'localhost.localdomain' ]];
+  then
+    break
+  fi
+  sleep 2
+done
+
+while true; do
+  FQHOSTNAME=$(hostname -f)
+  echo "Setting static hostname to $FQHOSTNAME"
+  hostnamectl set-hostname $FQHOSTNAME
+  if [[ $? -eq 0 ]]; then
+    break;
+  fi
+done
+
+while true;
+do
   if [[ -L "/sys/class/net/vhost0" && $(ip address show vhost0 | grep inet[^6]) ]];
   then
           echo "[INFO] Detected vhost0 interface. Stopping NetworkManager..."
