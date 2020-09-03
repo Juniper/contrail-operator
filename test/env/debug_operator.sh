ac83733f323d3fb73541c05b3e87c864633bb663
Michal Skalski <mskalski@juniper.net>
2020-09-03 12:28:46 +0200

    Add some info how to debug operator in VSCode (#347)

---
+++ test/env/debug_operator.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# This script will run Contrail operator locally with delve debugger enabled.
+# See DEVELOPMENT.md for more details.
+set -e
+
+DIR="$(dirname "${BASH_SOURCE[0]}")"
+
+${DIR}/../../deploy/create_manifest.sh -l
+kubectl apply -f ${DIR}/../../deploy/local_operator.yaml
+kubectl apply -f ${DIR}/deploy/secret.yaml
+kubectl apply -f ${DIR}/deploy/cluster.yaml
+
+pushd ${DIR}/../../
+operator-sdk run --local --watch-namespace contrail --enable-delve
+popd
