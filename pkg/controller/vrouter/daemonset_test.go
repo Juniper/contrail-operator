2396ca2dbee46bfd1c0520a79c8881ec6a20eb36
Dysproz <krasuski.szymon.piotr@gmail.com>
2020-09-04 09:02:31 +0200

    Format daemonset vrouter tests

--- pkg/controller/vrouter/daemonset_test.go
+++ pkg/controller/vrouter/daemonset_test.go
@@ -1,12 +1,12 @@
 package vrouter_test
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"testing"
 
 	"github.com/Juniper/contrail-operator/pkg/controller/vrouter"
 )
 
 func TestGetDaemonset(t *testing.T) {
-	assert.NotPanics(t, func() {_ = vrouter.GetDaemonset()}, "Daemonset got properly")
+	assert.NotPanics(t, func() { _ = vrouter.GetDaemonset() }, "Daemonset got properly")
 }
