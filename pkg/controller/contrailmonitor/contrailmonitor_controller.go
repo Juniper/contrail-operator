16f1da45c1a34a0437352db49caf367f00515a20
vijayrap <vraparla@juniper.net>
2020-09-09 07:27:30 -0700

    removed comment

--- pkg/controller/contrailmonitor/contrailmonitor_controller.go
+++ pkg/controller/contrailmonitor/contrailmonitor_controller.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 
 	contrailv1alpha1 "github.com/Juniper/contrail-operator/pkg/apis/contrail/v1alpha1"
-	// corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
