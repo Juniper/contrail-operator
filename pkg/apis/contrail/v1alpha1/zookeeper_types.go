59b444bbe989ea71bdc686db626c8395924780e6
Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
2020-09-09 10:01:28 +0200

    Change storage Path in Zookeeper and Cassandra to /mnt subpath

--- pkg/apis/contrail/v1alpha1/zookeeper_types.go
+++ pkg/apis/contrail/v1alpha1/zookeeper_types.go
@@ -272,7 +272,7 @@ func (c *Zookeeper) ConfigurationParameters() interface{} {
 	var electionPort int
 	var serverPort int
 	if c.Spec.ServiceConfiguration.Storage.Path == "" {
-		zookeeperConfiguration.Storage.Path = "/var/lib/zookeeper"
+		zookeeperConfiguration.Storage.Path = "/mnt/zookeeper"
 	} else {
 		zookeeperConfiguration.Storage.Path = c.Spec.ServiceConfiguration.Storage.Path
 	}
