f184164e8861b0d4706ae99c3a6258c76ce3c7d2
Dysproz <krasuski.szymon.piotr@gmail.com>
2020-09-16 15:33:19 +0200

    Copy file

--- deploy/openshift/sources/link_multus_config.sh
+++ deploy/openshift/sources/link_multus_config.sh
@@ -4,4 +4,4 @@ while [ ! -f /etc/cni/net.d/10-contrail.conf ]
 do
   sleep 2
 done
-ln -s /etc/cni/net.d/10-contrail.conf /var/run/multus/cni/net.d/80-openshift-network.conf
+cp -f /etc/cni/net.d/10-contrail.conf /var/run/multus/cni/net.d/80-openshift-network.conf
