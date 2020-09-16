938432af074f2d299d1026494b158310813470d0
Dysproz <krasuski.szymon.piotr@gmail.com>
2020-09-16 16:24:30 +0200

    Use hardlink

--- deploy/openshift/sources/link_multus_config.sh
+++ deploy/openshift/sources/link_multus_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-while [ ! -f /etc/cni/net.d/10-contrail.conf ]
+while [ ! -f /tmp/list.txt ]
 do
   sleep 2
 done
-cp -f /etc/cni/net.d/10-contrail.conf /var/run/multus/cni/net.d/80-openshift-network.conf
+ln /etc/cni/net.d/10-contrail.conf /var/run/multus/cni/net.d/80-openshift-network.conf
