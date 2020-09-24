12d0cb9508ce72d276e9544db03994e36ac6cb36
Piotr Sykulski <psykulski@juniper.net>
2020-09-24 12:49:14 +0200

    Fixed order of checks in controller

--- pkg/controller/vrouter/vrouter_controller.go
+++ pkg/controller/vrouter/vrouter_controller.go
@@ -188,15 +188,11 @@ func (r *ReconcileVrouter) Reconcile(request reconcile.Request) (reconcile.Resul
 		return reconcile.Result{}, nil
 	}
 
-	if !r.vrouterDependenciesReady(instance, request.Namespace) {
-		return reconcile.Result{}, nil
-	}
-
-	if err := r.Client.Get(context.TODO(), request.NamespacedName, instance); err != nil && errors.IsNotFound(err) {
-		return reconcile.Result{}, nil
-	}
-
 	if !instance.GetDeletionTimestamp().IsZero() {
+		return reconcile.Result{}, nil
+	}
+
+	if !r.vrouterDependenciesReady(instance, request.Namespace) {
 		return reconcile.Result{}, nil
 	}
 
