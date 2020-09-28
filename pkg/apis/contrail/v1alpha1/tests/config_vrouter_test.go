b272521060e788c0dc40a9b8fb6ecccac78ac7e1
Piotr Sykulski <psykulski@juniper.net>
2020-09-28 17:05:46 +0200

    Modified vrotuer config unit tests to check specific fields in ini files inside the whole config.

--- pkg/apis/contrail/v1alpha1/tests/config_vrouter_test.go
+++ pkg/apis/contrail/v1alpha1/tests/config_vrouter_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/kylelemons/godebug/diff"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/ini.v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
@@ -154,10 +155,16 @@ func TestVrouterConfigStaticConfiguration(t *testing.T) {
 				if err := cl.Get(context.TODO(), types.NamespacedName{Name: "vrouter1-vrouter-configmap", Namespace: "default"}, &environment.vrouterConfigMap); err != nil {
 					t.Fatalf("get configmap: (%v)", err)
 				}
-				if environment.vrouterConfigMap.Data["vrouter.1.1.8.1"] != vrouterConfigStaticConfigurationConfigControlDefaultPorts {
-					configDiff := diff.Diff(environment.vrouterConfigMap.Data["vrouter.1.1.8.1"], vrouterConfigStaticConfigurationConfigControlDefaultPorts)
-					t.Fatalf("get vrouter config: \n%v\n", configDiff)
+				vrouterConfig, err := ini.Load([]byte(environment.vrouterConfigMap.Data["vrouter.1.1.8.1"]))
+				if err != nil {
+					t.Fatalf("failed to read vrouter config as ini file: (%v)", err)
 				}
+				expectedControlXMPPEpointsList := "1.1.1.1:5269 2.2.2.2:5269 3.3.3.3:5269"
+				assert.Equal(t, expectedControlXMPPEpointsList, vrouterConfig.Section("CONTROL-NODE").Key("servers").String())
+				expectedCollectorEndpointsList := "1.1.1.1:8086 2.2.2.2:8086 3.3.3.3:8086"
+				assert.Equal(t, expectedCollectorEndpointsList, vrouterConfig.Section("DEFAULT").Key("collectors").String())
+				expectedDNSEndpointsList := "1.1.1.1:53 2.2.2.2:53 3.3.3.3:53"
+				assert.Equal(t, expectedDNSEndpointsList, vrouterConfig.Section("DNS").Key("servers").String())
 			})
 		})
 		t.Run("when Vrouter has StaticConfiguration filled for both config and control with non default ports", func(t *testing.T) {
@@ -188,10 +195,16 @@ func TestVrouterConfigStaticConfiguration(t *testing.T) {
 				if err := cl.Get(context.TODO(), types.NamespacedName{Name: "vrouter1-vrouter-configmap", Namespace: "default"}, &environment.vrouterConfigMap); err != nil {
 					t.Fatalf("get configmap: (%v)", err)
 				}
-				if environment.vrouterConfigMap.Data["vrouter.1.1.8.1"] != vrouterConfigStaticConfigurationConfigControlNonDefaultPorts {
-					configDiff := diff.Diff(environment.vrouterConfigMap.Data["vrouter.1.1.8.1"], vrouterConfigStaticConfigurationConfigControlNonDefaultPorts)
-					t.Fatalf("get vrouter config: \n%v\n", configDiff)
+				vrouterConfig, err := ini.Load([]byte(environment.vrouterConfigMap.Data["vrouter.1.1.8.1"]))
+				if err != nil {
+					t.Fatalf("failed to read vrouter config as ini file: (%v)", err)
 				}
+				expectedControlXMPPEpointsList := "1.1.1.1:5 2.2.2.2:5 3.3.3.3:5"
+				assert.Equal(t, expectedControlXMPPEpointsList, vrouterConfig.Section("CONTROL-NODE").Key("servers").String())
+				expectedCollectorEndpointsList := "1.1.1.1:3 2.2.2.2:3 3.3.3.3:3"
+				assert.Equal(t, expectedCollectorEndpointsList, vrouterConfig.Section("DEFAULT").Key("collectors").String())
+				expectedDNSEndpointsList := "1.1.1.1:7 2.2.2.2:7 3.3.3.3:7"
+				assert.Equal(t, expectedDNSEndpointsList, vrouterConfig.Section("DNS").Key("servers").String())
 			})
 		})
 		t.Run("when Vrouter has StaticConfiguration filled for only the config", func(t *testing.T) {
@@ -211,10 +224,16 @@ func TestVrouterConfigStaticConfiguration(t *testing.T) {
 				if err := cl.Get(context.TODO(), types.NamespacedName{Name: "vrouter1-vrouter-configmap", Namespace: "default"}, &environment.vrouterConfigMap); err != nil {
 					t.Fatalf("get configmap: (%v)", err)
 				}
-				if environment.vrouterConfigMap.Data["vrouter.1.1.8.1"] != vrouterConfigStaticConfigurationConfigDefaultPorts {
-					configDiff := diff.Diff(environment.vrouterConfigMap.Data["vrouter.1.1.8.1"], vrouterConfigStaticConfigurationConfigDefaultPorts)
-					t.Fatalf("get vrouter config: \n%v\n", configDiff)
+				vrouterConfig, err := ini.Load([]byte(environment.vrouterConfigMap.Data["vrouter.1.1.8.1"]))
+				if err != nil {
+					t.Fatalf("failed to read vrouter config as ini file: (%v)", err)
 				}
+				expectedControlXMPPEpointsList := "1.1.5.1:5269 1.1.5.2:5269 1.1.5.3:5269"
+				assert.Equal(t, expectedControlXMPPEpointsList, vrouterConfig.Section("CONTROL-NODE").Key("servers").String())
+				expectedCollectorEndpointsList := "1.1.1.1:8086 2.2.2.2:8086 3.3.3.3:8086"
+				assert.Equal(t, expectedCollectorEndpointsList, vrouterConfig.Section("DEFAULT").Key("collectors").String())
+				expectedDNSEndpointsList := "1.1.5.1:53 1.1.5.2:53 1.1.5.3:53"
+				assert.Equal(t, expectedDNSEndpointsList, vrouterConfig.Section("DNS").Key("servers").String())
 			})
 		})
 		t.Run("when Vrouter has StaticConfiguration filled for only control", func(t *testing.T) {
@@ -232,10 +251,16 @@ func TestVrouterConfigStaticConfiguration(t *testing.T) {
 				if err := cl.Get(context.TODO(), types.NamespacedName{Name: "vrouter1-vrouter-configmap", Namespace: "default"}, &environment.vrouterConfigMap); err != nil {
 					t.Fatalf("get configmap: (%v)", err)
 				}
-				if environment.vrouterConfigMap.Data["vrouter.1.1.8.1"] != vrouterConfigStaticConfigurationControlDefaultPorts {
-					configDiff := diff.Diff(environment.vrouterConfigMap.Data["vrouter.1.1.8.1"], vrouterConfigStaticConfigurationControlDefaultPorts)
-					t.Fatalf("get vrouter config: \n%v\n", configDiff)
+				vrouterConfig, err := ini.Load([]byte(environment.vrouterConfigMap.Data["vrouter.1.1.8.1"]))
+				if err != nil {
+					t.Fatalf("failed to read vrouter config as ini file: (%v)", err)
 				}
+				expectedControlXMPPEpointsList := "1.1.1.1:5269 2.2.2.2:5269 3.3.3.3:5269"
+				assert.Equal(t, expectedControlXMPPEpointsList, vrouterConfig.Section("CONTROL-NODE").Key("servers").String())
+				expectedCollectorEndpointsList := "1.1.1.1:8086 1.1.1.2:8086 1.1.1.3:8086"
+				assert.Equal(t, expectedCollectorEndpointsList, vrouterConfig.Section("DEFAULT").Key("collectors").String())
+				expectedDNSEndpointsList := "1.1.1.1:53 2.2.2.2:53 3.3.3.3:53"
+				assert.Equal(t, expectedDNSEndpointsList, vrouterConfig.Section("DNS").Key("servers").String())
 			})
 		})
 	})
@@ -269,194 +294,6 @@ sandesh_ca_cert=/etc/ssl/certs/kubernetes/ca-bundle.crt
 control_network_ip=1.1.8.1
 [DNS]
 servers=1.1.5.1:53 1.1.5.2:53 1.1.5.3:53
-[METADATA]
-metadata_proxy_secret=contrail
-[VIRTUAL-HOST-INTERFACE]
-name=vhost0
-ip=1.1.8.1/24
-physical_interface=eth0
-compute_node_address=1.1.8.1
-gateway=1.1.8.254
-[SERVICE-INSTANCE]
-netns_command=/usr/bin/opencontrail-vrouter-netns
-docker_command=/usr/bin/opencontrail-vrouter-docker
-[HYPERVISOR]
-type = kvm
-[FLOWS]
-fabric_snat_hash_table_size = 4096
-[SESSION]
-slo_destination = collector
-sample_destination = collector`
-
-var vrouterConfigStaticConfigurationConfigControlDefaultPorts = `[CONTROL-NODE]
-servers=1.1.1.1:5269 2.2.2.2:5269 3.3.3.3:5269
-[DEFAULT]
-http_server_ip=0.0.0.0
-collectors=1.1.1.1:8086 2.2.2.2:8086 3.3.3.3:8086
-log_file=/var/log/contrail/contrail-vrouter-agent.log
-log_level=SYS_NOTICE
-log_local=1
-hostname=host1
-agent_name=host1
-xmpp_dns_auth_enable=True
-xmpp_auth_enable=True
-xmpp_server_cert=/etc/certificates/server-1.1.8.1.crt
-xmpp_server_key=/etc/certificates/server-key-1.1.8.1.pem
-xmpp_ca_cert=/etc/ssl/certs/kubernetes/ca-bundle.crt
-physical_interface_mac = de:ad:be:ef:ba:be
-tsn_servers = []
-[SANDESH]
-introspect_ssl_enable=True
-introspect_ssl_insecure=True
-sandesh_ssl_enable=True
-sandesh_keyfile=/etc/certificates/server-key-1.1.8.1.pem
-sandesh_certfile=/etc/certificates/server-1.1.8.1.crt
-sandesh_ca_cert=/etc/ssl/certs/kubernetes/ca-bundle.crt
-[NETWORKS]
-control_network_ip=1.1.8.1
-[DNS]
-servers=1.1.1.1:53 2.2.2.2:53 3.3.3.3:53
-[METADATA]
-metadata_proxy_secret=contrail
-[VIRTUAL-HOST-INTERFACE]
-name=vhost0
-ip=1.1.8.1/24
-physical_interface=eth0
-compute_node_address=1.1.8.1
-gateway=1.1.8.254
-[SERVICE-INSTANCE]
-netns_command=/usr/bin/opencontrail-vrouter-netns
-docker_command=/usr/bin/opencontrail-vrouter-docker
-[HYPERVISOR]
-type = kvm
-[FLOWS]
-fabric_snat_hash_table_size = 4096
-[SESSION]
-slo_destination = collector
-sample_destination = collector`
-
-var vrouterConfigStaticConfigurationConfigControlNonDefaultPorts = `[CONTROL-NODE]
-servers=1.1.1.1:5 2.2.2.2:5 3.3.3.3:5
-[DEFAULT]
-http_server_ip=0.0.0.0
-collectors=1.1.1.1:3 2.2.2.2:3 3.3.3.3:3
-log_file=/var/log/contrail/contrail-vrouter-agent.log
-log_level=SYS_NOTICE
-log_local=1
-hostname=host1
-agent_name=host1
-xmpp_dns_auth_enable=True
-xmpp_auth_enable=True
-xmpp_server_cert=/etc/certificates/server-1.1.8.1.crt
-xmpp_server_key=/etc/certificates/server-key-1.1.8.1.pem
-xmpp_ca_cert=/etc/ssl/certs/kubernetes/ca-bundle.crt
-physical_interface_mac = de:ad:be:ef:ba:be
-tsn_servers = []
-[SANDESH]
-introspect_ssl_enable=True
-introspect_ssl_insecure=True
-sandesh_ssl_enable=True
-sandesh_keyfile=/etc/certificates/server-key-1.1.8.1.pem
-sandesh_certfile=/etc/certificates/server-1.1.8.1.crt
-sandesh_ca_cert=/etc/ssl/certs/kubernetes/ca-bundle.crt
-[NETWORKS]
-control_network_ip=1.1.8.1
-[DNS]
-servers=1.1.1.1:7 2.2.2.2:7 3.3.3.3:7
-[METADATA]
-metadata_proxy_secret=contrail
-[VIRTUAL-HOST-INTERFACE]
-name=vhost0
-ip=1.1.8.1/24
-physical_interface=eth0
-compute_node_address=1.1.8.1
-gateway=1.1.8.254
-[SERVICE-INSTANCE]
-netns_command=/usr/bin/opencontrail-vrouter-netns
-docker_command=/usr/bin/opencontrail-vrouter-docker
-[HYPERVISOR]
-type = kvm
-[FLOWS]
-fabric_snat_hash_table_size = 4096
-[SESSION]
-slo_destination = collector
-sample_destination = collector`
-
-var vrouterConfigStaticConfigurationConfigDefaultPorts = `[CONTROL-NODE]
-servers=1.1.5.1:5269 1.1.5.2:5269 1.1.5.3:5269
-[DEFAULT]
-http_server_ip=0.0.0.0
-collectors=1.1.1.1:8086 2.2.2.2:8086 3.3.3.3:8086
-log_file=/var/log/contrail/contrail-vrouter-agent.log
-log_level=SYS_NOTICE
-log_local=1
-hostname=host1
-agent_name=host1
-xmpp_dns_auth_enable=True
-xmpp_auth_enable=True
-xmpp_server_cert=/etc/certificates/server-1.1.8.1.crt
-xmpp_server_key=/etc/certificates/server-key-1.1.8.1.pem
-xmpp_ca_cert=/etc/ssl/certs/kubernetes/ca-bundle.crt
-physical_interface_mac = de:ad:be:ef:ba:be
-tsn_servers = []
-[SANDESH]
-introspect_ssl_enable=True
-introspect_ssl_insecure=True
-sandesh_ssl_enable=True
-sandesh_keyfile=/etc/certificates/server-key-1.1.8.1.pem
-sandesh_certfile=/etc/certificates/server-1.1.8.1.crt
-sandesh_ca_cert=/etc/ssl/certs/kubernetes/ca-bundle.crt
-[NETWORKS]
-control_network_ip=1.1.8.1
-[DNS]
-servers=1.1.5.1:53 1.1.5.2:53 1.1.5.3:53
-[METADATA]
-metadata_proxy_secret=contrail
-[VIRTUAL-HOST-INTERFACE]
-name=vhost0
-ip=1.1.8.1/24
-physical_interface=eth0
-compute_node_address=1.1.8.1
-gateway=1.1.8.254
-[SERVICE-INSTANCE]
-netns_command=/usr/bin/opencontrail-vrouter-netns
-docker_command=/usr/bin/opencontrail-vrouter-docker
-[HYPERVISOR]
-type = kvm
-[FLOWS]
-fabric_snat_hash_table_size = 4096
-[SESSION]
-slo_destination = collector
-sample_destination = collector`
-
-var vrouterConfigStaticConfigurationControlDefaultPorts = `[CONTROL-NODE]
-servers=1.1.1.1:5269 2.2.2.2:5269 3.3.3.3:5269
-[DEFAULT]
-http_server_ip=0.0.0.0
-collectors=1.1.1.1:8086 1.1.1.2:8086 1.1.1.3:8086
-log_file=/var/log/contrail/contrail-vrouter-agent.log
-log_level=SYS_NOTICE
-log_local=1
-hostname=host1
-agent_name=host1
-xmpp_dns_auth_enable=True
-xmpp_auth_enable=True
-xmpp_server_cert=/etc/certificates/server-1.1.8.1.crt
-xmpp_server_key=/etc/certificates/server-key-1.1.8.1.pem
-xmpp_ca_cert=/etc/ssl/certs/kubernetes/ca-bundle.crt
-physical_interface_mac = de:ad:be:ef:ba:be
-tsn_servers = []
-[SANDESH]
-introspect_ssl_enable=True
-introspect_ssl_insecure=True
-sandesh_ssl_enable=True
-sandesh_keyfile=/etc/certificates/server-key-1.1.8.1.pem
-sandesh_certfile=/etc/certificates/server-1.1.8.1.crt
-sandesh_ca_cert=/etc/ssl/certs/kubernetes/ca-bundle.crt
-[NETWORKS]
-control_network_ip=1.1.8.1
-[DNS]
-servers=1.1.1.1:53 2.2.2.2:53 3.3.3.3:53
 [METADATA]
 metadata_proxy_secret=contrail
 [VIRTUAL-HOST-INTERFACE]
