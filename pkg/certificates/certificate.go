da2d1b16cb2bb692708ad5c292d9052a75e3c682
Paweł Marchewka <pawel.marchewka@codilime.com>
2020-11-12 17:34:38 +0100

    Create certs for all running pods with IP and don't create certificates for "Failed' pods. (#457)

--- pkg/certificates/certificate.go
+++ pkg/certificates/certificate.go
@@ -53,12 +53,19 @@ func (r *Certificate) FillSecret(secret *core.Secret) error {
 
 	for _, subject := range r.certificateSubjects {
 		if subject.ip == "" {
-			return fmt.Errorf("%s subject IP still no available", subject.name)
+			continue
 		}
 		if err := r.createCertificateForPod(subject, secret); err != nil {
 			return err
 		}
 	}
+
+	for _, subject := range r.certificateSubjects {
+		if subject.ip == "" {
+			return fmt.Errorf("%s subject IP still no available", subject.name)
+		}
+	}
+
 	return nil
 }
 
