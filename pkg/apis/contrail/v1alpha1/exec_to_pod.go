de7424e874804f6ac002b702efe7d927064824b4
Janek3d <mikluszka.janusz@gmail.com>
2020-11-18 17:51:14 +0100

    Change GetClientConfig function to get config from config.GetConfig

--- pkg/apis/contrail/v1alpha1/exec_to_pod.go
+++ pkg/apis/contrail/v1alpha1/exec_to_pod.go
@@ -4,81 +4,26 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"os"
-	"os/user"
-	"path/filepath"
 
 	core_v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
 
 	// "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/remotecommand"
 )
 
 const debug = false
-
-// GetConfig creates a *rest.Config for talking to a Kubernetes apiserver.
-// If --kubeconfig is set, will use the kubeconfig file at that location.  Otherwise will assume running
-// in cluster and use the cluster provided kubeconfig.
-//
-// Config precedence
-//
-// * --kubeconfig flag pointing at a file
-//
-// * KUBECONFIG environment variable pointing at a file
-//
-// * In-cluster config if running in cluster
-//
-// * $HOME/.kube/config if exists
-func GetConfig() (*rest.Config, error) {
-	var kubeconfig, masterURL string
-	// If a flag is specified with the config location, use that
-	if len(kubeconfig) > 0 {
-		return clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
-	}
-	// If an env variable is specified with the config locaiton, use that
-	if len(os.Getenv("KUBECONFIG")) > 0 {
-		return clientcmd.BuildConfigFromFlags(masterURL, os.Getenv("KUBECONFIG"))
-	}
-	// If no explicit location, try the in-cluster config
-	if c, err := rest.InClusterConfig(); err == nil {
-		return c, nil
-	}
-	// If no in-cluster config, try the default location in the user's home directory
-	if usr, err := user.Current(); err == nil {
-		if c, err := clientcmd.BuildConfigFromFlags(
-			"", filepath.Join(usr.HomeDir, ".kube", "config")); err == nil {
-			return c, nil
-		}
-	}
-
-	return nil, fmt.Errorf("could not locate a kubeconfig")
-}
 
 // GetClientConfig first tries to get a config object which uses the service account kubernetes gives to pods,
 // if it is called from a process running in a kubernetes environment.
 // Otherwise, it tries to build config from a default kubeconfig filepath if it fails, it fallback to the default config.
 // Once it get the config, it returns the same.
 func GetClientConfig() (*rest.Config, error) {
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		if debug {
-			fmt.Printf("Unable to create config. Error: %+v\n", err)
-		}
-		err1 := err
-		kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
-		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
-		if err != nil {
-			err = fmt.Errorf("InClusterConfig as well as BuildConfigFromFlags Failed. Error in InClusterConfig: %+v\nError in BuildConfigFromFlags: %+v", err1, err)
-			return nil, err
-		}
-	}
-
-	return config, nil
+	return config.GetConfig()
 }
 
 // GetClientsetFromConfig takes REST config and Create a clientset based on that and return that clientset.
