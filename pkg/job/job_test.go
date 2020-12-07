3e3a45d764685e9fcb8c2c8c17f27dae9c94a0a9
Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
2020-12-07 15:20:34 +0100

    Check if no. of failed pods exceeded backofflimit

--- pkg/job/job_test.go
+++ pkg/job/job_test.go
@@ -12,62 +12,80 @@ import (
 
 func TestStatus_Pending(t *testing.T) {
 	tests := map[string]struct {
-		jobStatus       batch.JobStatus
+		job             batch.Job
 		expectedPending bool
 	}{
 		"no conditions": {
-			jobStatus: batch.JobStatus{
-				Conditions: []batch.JobCondition{},
+			job: batch.Job{
+				Status: batch.JobStatus{
+					Conditions: []batch.JobCondition{},
+				},
 			},
 			expectedPending: true,
 		},
 		"nil conditions": {
-			jobStatus: batch.JobStatus{
-				Conditions: nil,
+			job: batch.Job{
+				Status: batch.JobStatus{
+					Conditions: nil,
+				},
 			},
 			expectedPending: true,
 		},
 		"condition type Failed, status True": {
-			jobStatus: batch.JobStatus{
-				Conditions: []batch.JobCondition{{Type: batch.JobFailed, Status: v1.ConditionTrue}},
+			job: batch.Job{
+				Status: batch.JobStatus{
+					Conditions: []batch.JobCondition{{Type: batch.JobFailed, Status: v1.ConditionTrue}},
+				},
 			},
 			expectedPending: false,
 		},
 		"condition type Failed, status False": {
-			jobStatus: batch.JobStatus{
-				Conditions: []batch.JobCondition{{Type: batch.JobFailed, Status: v1.ConditionFalse}},
+			job: batch.Job{
+				Status: batch.JobStatus{
+					Conditions: []batch.JobCondition{{Type: batch.JobFailed, Status: v1.ConditionFalse}},
+				},
 			},
 			expectedPending: true,
 		},
 		"condition type fake, status true": {
-			jobStatus: batch.JobStatus{
-				Conditions: []batch.JobCondition{{Type: "fake", Status: v1.ConditionTrue}},
+			job: batch.Job{
+				Status: batch.JobStatus{
+					Conditions: []batch.JobCondition{{Type: "fake", Status: v1.ConditionTrue}},
+				},
 			},
 			expectedPending: true,
 		},
 		"condition type fake, status false": {
-			jobStatus: batch.JobStatus{
-				Conditions: []batch.JobCondition{{Type: "fake", Status: v1.ConditionFalse}},
+			job: batch.Job{
+				Status: batch.JobStatus{
+					Conditions: []batch.JobCondition{{Type: "fake", Status: v1.ConditionFalse}},
+				},
 			},
 			expectedPending: true,
 		},
 		"condition type Complete, status True": {
-			jobStatus: batch.JobStatus{
-				Conditions: []batch.JobCondition{{Type: batch.JobComplete, Status: v1.ConditionTrue}},
+			job: batch.Job{
+				Status: batch.JobStatus{
+					Conditions: []batch.JobCondition{{Type: batch.JobComplete, Status: v1.ConditionTrue}},
+				},
 			},
 			expectedPending: false,
 		},
 		"condition type Complete, status False": {
-			jobStatus: batch.JobStatus{
-				Conditions: []batch.JobCondition{{Type: batch.JobComplete, Status: v1.ConditionFalse}},
+			job: batch.Job{
+				Status: batch.JobStatus{
+					Conditions: []batch.JobCondition{{Type: batch.JobComplete, Status: v1.ConditionFalse}},
+				},
 			},
 			expectedPending: true,
 		},
 		"multiple conditions": {
-			jobStatus: batch.JobStatus{
-				Conditions: []batch.JobCondition{
-					{Type: batch.JobComplete, Status: v1.ConditionFalse},
-					{Type: batch.JobFailed, Status: v1.ConditionTrue},
+			job: batch.Job{
+				Status: batch.JobStatus{
+					Conditions: []batch.JobCondition{
+						{Type: batch.JobComplete, Status: v1.ConditionFalse},
+						{Type: batch.JobFailed, Status: v1.ConditionTrue},
+					},
 				},
 			},
 			expectedPending: false,
@@ -76,7 +94,7 @@ func TestStatus_Pending(t *testing.T) {
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
 			// when
-			pending := job.Status(test.jobStatus).JobPending()
+			pending := job.Job(test.job).JobPending()
 			// then
 			assert.Equal(t, test.expectedPending, pending)
 		})
