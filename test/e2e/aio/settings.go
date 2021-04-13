47d4e71576247b2743b1e6760c35f2a09d11d375
vraparla <vraparla@vraparla-mbp.jnpr.net>
2021-04-13 13:23:29 -0700

    Increasing the timeout as jenkins fails

--- test/e2e/aio/settings.go
+++ test/e2e/aio/settings.go
@@ -3,9 +3,9 @@ package e2e
 import "time"
 
 var (
-	retryInterval          = time.Second * 5
-	waitTimeout            = time.Second * 400
+	retryInterval          = time.Second * 15
+	waitTimeout            = time.Second * 800
 	cleanupRetryInterval   = time.Second * 1
 	cleanupTimeout         = time.Second * 5
-	waitForOperatorTimeout = time.Minute * 10
+	waitForOperatorTimeout = time.Minute * 30
 )
